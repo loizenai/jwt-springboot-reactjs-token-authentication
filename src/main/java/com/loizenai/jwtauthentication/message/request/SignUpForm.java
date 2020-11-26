b8e05afb1be2c6c55ca29d2456f305b77bcc82d1
Mona Lisa <leloizenai@gmail.com>
2020-11-26 23:21:09 +0700

    jwt springboot reactjs token authentication example

---
+++ src/main/java/com/loizenai/jwtauthentication/message/request/SignUpForm.java
@@ -0,0 +1,78 @@
+package com.loizenai.jwtauthentication.message.request;
+
+import java.util.Set;
+
+import javax.validation.constraints.*;
+
+public class SignUpForm {
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String firstname;
+    
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String lastname;
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotBlank
+    @Size(max = 60)
+    @Email
+    private String email;
+    
+    private Set<String> role;
+    
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+    
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public Set<String> getRole() {
+    	return this.role;
+    }
+    
+    public void setRole(Set<String> role) {
+    	this.role = role;
+    }
+}
