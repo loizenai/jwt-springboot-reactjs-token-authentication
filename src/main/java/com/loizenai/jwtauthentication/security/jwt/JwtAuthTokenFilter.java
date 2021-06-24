26cd3bf569a3662863740f256f06cdb791d3f6b7
loizenai <leloizenai@gmail.com>
2021-06-24 14:21:36 +0700

    ok

--- src/main/java/com/loizenai/jwtauthentication/security/jwt/JwtAuthTokenFilter.java
+++ src/main/java/com/loizenai/jwtauthentication/security/jwt/JwtAuthTokenFilter.java
@@ -37,9 +37,9 @@ public class JwtAuthTokenFilter extends OncePerRequestFilter {
 			if (jwt != null && tokenProvider.validateJwtToken(jwt)) {
 				String username = tokenProvider.getUserNameFromJwtToken(jwt);
 
-				UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+				UserDetails _userDetails = userDetailsService.loadUserByUsername(username);
 				UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-						userDetails, null, userDetails.getAuthorities());
+						_userDetails, null, _userDetails.getAuthorities());
 				authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
 				SecurityContextHolder.getContext().setAuthentication(authentication);
